bfb68c9a938baa140e7302ae0d95f0f879175adc
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-08-06 21:57:55 -0600

    chore: Update service.js to include setupPlayer and addTrack functions

--- service.js
+++ service.js
@@ -1,5 +1,40 @@
-import TrackPlayer from "react-native-track-player";
+import TrackPlayer, { RepeatMode } from "react-native-track-player";
 import { playListData } from "./src/constants";
+
+export async function setupPlayer() {
+    let isSetup = false;
+    try {
+       await TrackPlayer.getCurrentTrack();
+       isSetup = true;
+    } catch (error) {
+        await TrackPlayer.setupPlayer();
+        isSetup = true;
+    } finally {
+        return isSetup;
+    }
+}
+
+export async function addTrack(){
+    await TrackPlayer.add(playListData); //This needs to be Track[]
+    await TrackPlayer.setRepeatMode(RepeatMode.Queue);
+}
+
+
 export default async function service() {
-  
+    
+    TrackPlayer.addEventListener(Event.RemotePause, () => {
+        TrackPlayer.pause();
+    }); 
+
+    TrackPlayer.addEventListener(Event.RemotePlay, () => {
+        TrackPlayer.play();
+    })
+
+    TrackPlayer.addEventListener(Event.RemoteNext, () => {
+        TrackPlayer.next();
+    })
+
+    TrackPlayer.addEventListener(Event.RemotePrevious, () => {
+        TrackPlayer.previous();
+    })
 }
