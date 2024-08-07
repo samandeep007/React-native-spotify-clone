fbe7cedafa71715e6fe7750c3304fb0c8342f67b
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-08-06 22:22:57 -0600

    chore: Update ControlCenter component to include playback controls and state management

--- src/components/ControlCenter.tsx
+++ src/components/ControlCenter.tsx
@@ -1,10 +1,26 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
-import TrackPlayer from 'react-native-track-player';
+import TrackPlayer, { State, usePlaybackState } from 'react-native-track-player';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import service from '../../service';
 export default function ControlCenter(): JSX.Element{
-  
+
+    const playbackState = usePlaybackState();
+    
+    const skipToNext = async () => await TrackPlayer.skipToNext();
+    const skipToPrevious = async() => await TrackPlayer.skipToPrevious();
+   
+    const togglePlayback = async (playback: State) => {
+        const currentTrack = await TrackPlayer.getActiveTrack();
+        if(currentTrack !== null){
+            if(playback === State.Paused || playback === State.Ready){
+                await TrackPlayer.play();
+            }
+            else {
+                await TrackPlayer.pause();
+            }
+        }
+    }
   
     return (
     <View>
