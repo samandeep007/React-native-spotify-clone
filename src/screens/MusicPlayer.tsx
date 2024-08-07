37ae842b28efad37e3343ee9d41c91cb3bfbf798
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-08-06 22:55:50 -0600

    feat: Add playListData constant for MusicPlayer component

--- src/screens/MusicPlayer.tsx
+++ src/screens/MusicPlayer.tsx
@@ -1,11 +1,29 @@
 import { Dimensions, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import TrackPlayer, {Event, Track, useTrackPlayerEvents} from 'react-native-track-player';
+
+import { playListData } from '../constants';
+
+import SongInfo from '../components/SongInfo';
+import SongSlider from '../components/SongSlider';
+import ControlCenter from '../components/ControlCenter';
+
 
 const {width} = Dimensions.get('window');
 
 export default function MusicPlayer() {
-  return (
+
+    const[track, setTrack] = useState<Track | null>();
+
+    const renderArtWork = () => {
+        return(
+            <View style={styles.listArtWrapper}>
+                
+            </View>
+        )
+    }
+
+    return (
     <View>
       <Text>MusicPlayer</Text>
     </View>
