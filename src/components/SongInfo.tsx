dc5d5ca9eeac1e3d1e98d675c00db71dfc0b8ac0
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-08-06 22:38:18 -0600

    chore: Add SongInfo component

---
+++ src/components/SongInfo.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function SongInfo(): JSX.Element {
+  return (
+    <View>d
+      <Text>SongInfo</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
