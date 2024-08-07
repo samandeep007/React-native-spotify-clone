0bae88fb77898d158fafc702dce8a3e9c2c76a20
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-08-06 18:58:50 -0600

    chore: Register playback service with react-native-track-player

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
 import TrackPlayer from 'react-native-track-player';
+import service from './service';
 
 AppRegistry.registerComponent(appName, () => App);
-TrackPlayer.registerPlaybackService(() => require('./service'));
+TrackPlayer.registerPlaybackService(() => service);
