21782da9a44f05e057e3e2b226561091a6bc82ef
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-08-06 22:17:24 -0600

    chore: Update ControlCenter component to import necessary dependencies and add basic structure

---
+++ src/components/ControlCenter.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import TrackPlayer from 'react-native-track-player';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import service from '../../service';
+export default function ControlCenter(): JSX.Element{
+  
+  
+    return (
+    <View>
+     
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
